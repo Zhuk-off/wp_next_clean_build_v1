5100f447a6d6a1dab1997273c32baf7530a0feaf
Alexander <zhukoff.alexander@gmail.com>
2022-11-24 22:04:42 +0300

    chore: Add dependencies SCSS, Tailwind

--- next.config.js
+++ next.config.js
@@ -4,4 +4,20 @@ const nextConfig = {
   swcMinify: true,
 }
 
+const path = require('path')
+module.exports = {
+	trailingSlash: false,
+	webpackDevMiddleware: config => {
+		config.watchOptions = {
+			poll: 1000,
+			aggregateTimeout: 300
+		}
+
+		return config
+	},
+	sassOptions: {
+		includePaths: [path.join(__dirname, 'styles')]
+	}
+}
+
 module.exports = nextConfig
